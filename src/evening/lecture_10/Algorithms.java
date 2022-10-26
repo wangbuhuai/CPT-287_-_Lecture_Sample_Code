2da8919ed71dbc25c2b688208b95c547f8db6ea9
Dayu Wang <dwang@stchas.edu>
2022-10-26 18:10:26 -0500

    1026

--- src/evening/lecture_10/Algorithms.java
+++ src/evening/lecture_10/Algorithms.java
@@ -1,6 +1,6 @@
 // Created by Dayu Wang (dwang@stchas.edu) on 2022-10-19
 
-// Last updated by Dayu Wang (dwang@stchas.edu) on 2022-10-19
+// Last updated by Dayu Wang (dwang@stchas.edu) on 2022-10-26
 
 
 package evening.lecture_10;
@@ -10,25 +10,31 @@ import java.util.List;
 
 public class Algorithms {
     /** Converts a binary search tree to a sorted list.
-        @param root: reference to the root of a binary search tree
-        @return: sorted list
+        @param root: a reference to the root of a binary search tree
+        @return: a sorted list made from the binary search tree
     */
     public static <T> List<T> toSortedList(BTNode<T> root) {
+        // Base case
         if (root == null) { return new ArrayList<>(); }
+
+        // Inorder traversal
         List<T> result = toSortedList(root.left);
         result.add(root.data);
         result.addAll(toSortedList(root.right));
         return result;
     }  // Time complexity: O(n)
 
-    /** Converts a sorted list (from index start to end) to a binary search tree.
+    /** Converts a sorted list (from index start to end) to a balanced binary search tree.
         @param sortedList: a sorted list
         @param start: beginning index of the segment
         @param end: end index of the segment
         @return: a reference to the root of the binary search tree
     */
     private static <T> BTNode<T> toBST(List<T> sortedList, int start, int end) {
+        // Base case
         if (start > end) { return null; }
+
+        // Divide-and-conquer
         int mid = (start + end) / 2;
         BTNode<T> leftChild = toBST(sortedList, start, mid - 1);
         BTNode<T> rightChild = toBST(sortedList, mid + 1, end);
